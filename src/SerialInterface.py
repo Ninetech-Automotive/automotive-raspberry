fdcb3b1009bcb9f4630685b260df3f00ec5dfbb7
Jannis Zbinden <jannis.zbinden@proton.me>
2025-03-28 17:19:52 +0100

    removed unnecessary variable

--- src/SerialInterface.py
+++ src/SerialInterface.py
@@ -4,9 +4,8 @@ from Configuration.Configurator import Configurator
 
 class SerialInterface(CommunicationInterface):
     def __init__(self):
-        configurator = Configurator()
-        device = configurator.get_communication()["device"]
-        baud = configurator.get_communication()["baud"]
+        device = Configurator().get_communication()["device"]
+        baud = Configurator().get_communication()["baud"]
         self.serial = serial.Serial(device, baudrate=baud, timeout=1)
 
     def write(self, message):
